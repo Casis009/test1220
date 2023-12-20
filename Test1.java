ecf092a1ba21f75cd2ca24127a131c081e0c6f5d
casis009 <qorjs2003@naver.com>
2023-12-20 10:50:34 +0900

    메인클래스 가격 수정

--- Test1.java
+++ Test1.java
@@ -3,5 +3,8 @@ public class Test1{
     public static void main(String[] args){
         TestVO vo = new TestVO();
         vo.name = "마루치";
+
+        Test2 t2 = new Test2();
+        t2.price = 2500;
     }
 }
